58e8a26ad098ba47c56e94aa5fb6fc3900218ad6
MatthewRawson <mrawson@redev.co.uk>
2024-10-01 16:45:11 +0100

    Added Landing Page

---
+++ SupportTicketSystem/Pages/Index.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SupportTicketSystem.Pages;
+
+public partial class Index
+{
+
+    [Inject] NavigationManager navigationManager { get; set; }
+    private void NavigateToSupport()
+    {
+        navigationManager.NavigateTo("/support");
+    }
+}
